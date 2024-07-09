f7bd9af1bd08605dd27cf5e8c0f4ad06b3a9544c
Aurora Bailey <aurora.i.bailey@gmail.com>
2024-07-08 21:30:12 -0600

    add prerender

--- svelte.config.js
+++ svelte.config.js
@@ -7,17 +7,16 @@ const dev = process.argv.includes("dev");
 const config = {
   preprocess: preprocess(),
   kit: {
-    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-    // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter({
-      // default options are shown
       pages: "build",
       assets: "build",
-      fallback: "index.html",
+      fallback: "200.html", // Use '200.html' if you need a SPA fallback, or 'index.html' for static pages
     }),
     paths: {
-      base: "",
+      base: dev ? "" : "/neko-dreams", // Ensure this matches your GitHub repo name
+    },
+    prerender: {
+      entries: ["*"], // Prerender all routes
     },
   },
 };
