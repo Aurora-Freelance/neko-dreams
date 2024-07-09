bcb93aa959119f8c359598853e8176018b49bd20
Aurora Bailey <aurora.i.bailey@gmail.com>
2024-07-08 22:06:10 -0600

    add nojekyll file

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,18 @@
 import adapter from "@sveltejs/adapter-static";
-import preprocess from "svelte-preprocess";
-
+import { sveltePreprocess } from "svelte-preprocess";
 const dev = process.argv.includes("dev");
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: preprocess(),
+  preprocess: sveltePreprocess(),
   kit: {
     adapter: adapter({
       pages: "build",
       assets: "build",
-      fallback: "200.html", // Use '200.html' if you need a SPA fallback, or 'index.html' for static pages
+      fallback: "index.html", // Use '200.html' if you need a SPA fallback, or 'index.html' for static pages
     }),
     paths: {
-      base: dev ? "" : "/neko-dreams", // Ensure this matches your GitHub repo name
+      base: dev ? "" : "", // Ensure this matches your GitHub repo name
     },
     prerender: {
       entries: ["*"], // Prerender all routes
